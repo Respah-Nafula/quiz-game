d0cbac2ac061f972f27c6923ffc1d0bf150695b5
Respah-Nafula <nafularespah@gmail.com>
2022-04-23 19:18:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print( "welcome to my quiz game!" )
+playing = input( "Do yo want to play? ")
+if playing != "yes":
+    quit()
+    print("okay! lets play: )")
+answer = input("Is coding language Python, not a loosely typed language?")
+if answer=="no":
+    print('correct!')
+
+    answer == input("what does IP stand for in computing?")
+if answer== "internet protocol":
+    print('correct')
+    
+else:
+    print('incorrect')
+    answer== input ("what does GUI stand for in computing?")
+if answer =="graphical user interface":
+    print('correct')
+    
+else:
+        print('incorrect')
+        answer=("what does IOT stand for?")
+if answer== "internet of things":
+ print("correct")
+
