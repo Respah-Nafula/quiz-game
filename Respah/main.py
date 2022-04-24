d40ba05ad9d55b63880ed0fb86e3b3d6ce1b20ff
Respah-Nafula <nafularespah@gmail.com>
2022-04-24 22:17:17 +0300

    first commit

---
+++ Respah/main.py
@@ -0,0 +1,20 @@
+from ast import Num
+import numbers
+import random
+num = random.randrange(1,20)
+guess = int(input("guess  numbers between 1 and 20:"))
+while int  == numbers:
+    if guess < numbers:
+        print("you need to guess higher.Try again")
+        guess = int(input("\Guess a number between 1 and 50: "))
+else:
+    print("you need to guess lower. try again")
+    guess =int(input("\Guess numbers between 1 and 20: "))
+print ("Congratulations, you guessed the numbers correctly")
+
+
+
+
+
+
+ 
